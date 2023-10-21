e473ad067777bcf52831cad19787e89f70edffb5
VituXablau <victorcatharino17@gmail.com>
2023-10-21 00:52:13 -0300

    Combat2.1

--- My project/Assets/Scripts/Fish.cs
+++ My project/Assets/Scripts/Fish.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Fish : MonoBehaviour
 {
@@ -34,6 +35,9 @@ public class Fish : MonoBehaviour
                 Special();
             }
         }
+
+        if(life <= 0)
+        SceneManager.LoadScene("Selection");
     }
 
     public void Attack()
